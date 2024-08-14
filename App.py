b90a3822f261840222232b3b7e83c04cbf74efd1
liban0 <liban.abdulkadir@outlook.com>
2024-08-14 20:11:04 +0100

    Update App.py

--- App.py
+++ App.py
@@ -125,7 +125,7 @@ with st.sidebar:
     st.title("📊 Black-Scholes Model")
     st.write("`Created by:`")
     linkedin_url = "https://www.linkedin.com/in/liban-abdulkadir-731970195"
-    st.markdown(f'<a href="{linkedin_url}" target="_blank" style="text-decoration: none; color: inherit;"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="25" height="25" style="vertical-align: middle; margin-right: 10px;">`https://www.linkedin.com/in/liban-abdulkadir-731970195/overlay/about-this-profile/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BzF4ZA%2Fk8QnmtS6Pa%2FtO0oA%3D%3D`</a>', unsafe_allow_html=True)
+    st.markdown(f'<a href="{linkedin_url}" target="_blank" style="text-decoration: none; color: inherit;"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="25" height="25" style="vertical-align: middle; margin-right: 10px;">`Lban Abdulkadir`</a>', unsafe_allow_html=True)
 
     current_price = st.number_input("Current Asset Price", value=100.0)
     strike = st.number_input("Strike Price", value=100.0)
